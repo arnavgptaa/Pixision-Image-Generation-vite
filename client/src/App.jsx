429afef77fe724d25fd0ee6ba8e117c726221202
Arnav Gupta <arnavgpta@gmail.com>
2023-03-23 11:44:33 +0530

    tailwind, postcss, file-saver intallation

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/logo.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
