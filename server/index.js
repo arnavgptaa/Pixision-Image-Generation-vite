465e2bea185627309aeb20a9859c46ad3b458150
Arnav Gupta <arnavgpta@gmail.com>
2023-03-30 23:51:15 +0530

    backend started-callbacks

--- server/index.js
+++ server/index.js
@@ -1,3 +1,15 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+//additional middlewares
+app.use(cors());
+appsuse(express.json({ limit:'50mb' }));
+
+app.get('/' , async(req, res) => {
+    res.send('Hello from Pixision!');
+})
